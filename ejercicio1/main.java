3dbde18ce46768a15ccccbc472c7027c32e10560
Jose Monroy <mnry7999@gmail.com>
2022-11-11 19:31:05 -0600

    Creacion de instancias por medio estatico

--- ejercicio1/main.java
+++ ejercicio1/main.java
@@ -2,11 +2,19 @@ public class main {
     public static void main(String[] args) {
         Persona persona1 = new Persona("Juan", 20, 1.80, 80, true);
         Persona persona2 = new Persona("Pedro", 21, 1.70, 70, true);
-        System.out.println("Nombre: " + persona1.getName() + " Edad: " + persona1.getAge());
-        System.out.println("Nombre: " + persona2.getName() + " Edad: " + persona2.getAge());
-        Salon.PersonaNull(persona1);
-        Salon.PersonaNull(persona2);
-        System.out.println("Nombre: " + persona1.getName() + " Edad: " + persona1.getAge());
-        System.out.println("Nombre: " + persona2.getName() + " Edad: " + persona2.getAge());
+        Estudiante estudiante1 = new Estudiante(persona1);
+        Estudiante estudiante2 = new Estudiante(persona2);
+        //imprimri atributos de estudiante1)
+        System.out.println("Nombre: " + estudiante1.getPersona().getName());
+        System.out.println("Edad: " + estudiante1.getPersona().getAge());
+        System.out.println("Altura: " + estudiante1.getPersona().getHeight());
+
+    }
+    public static void compararInstancias( Persona persona1, Persona persona2){
+        if (persona1.equals(persona2)){
+            System.out.println("Las instancias son iguales");
+        }else{
+            System.out.println("Las instancias son diferentes");
+        }
     }
 }
