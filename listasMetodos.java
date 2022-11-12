4431c7cc1e90b99797b356e31e6a6fe003e2437c
Jose Monroy <mnry7999@gmail.com>
2022-11-12 09:16:43 -0600

    Ejercicio 1
    Rececpicon de 2 strings y 2 int y concatenacion de ambos

---
+++ listasMetodos.java
@@ -0,0 +1,8 @@
+public class listasMetodos{
+    public static void main (String[] args){
+        System.out.println(ejercicio1( "Hola", "Infotec", 3, 4));
+    }
+    public static String ejercicio1 (String string1, String string2, Integer int1, Integer int2){
+        return string1 + " " + string2 + " " + int1+ " " + int2;
+    }
+}
