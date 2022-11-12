e17658ef4341227f78dc972f8cfc7b4281ad9234
Jose Monroy <mnry7999@gmail.com>
2022-11-11 19:05:05 -0600

    Metodo para borrar atributos de los alumnos

---
+++ ejercicio1/Salon.java
@@ -0,0 +1,20 @@
+public class Salon{
+    static String Grupo;
+    static int NumAlumnos;
+    static double PromedioEdad;
+
+    public Salon(String grupo, int numAlumnos, double promedioEdad) {
+        Grupo = grupo;
+        NumAlumnos = numAlumnos;
+        PromedioEdad = promedioEdad;
+    }
+    static Persona PersonaNull (Persona persona){
+        persona.setName(null);
+        persona.setAge(0);
+        persona.setHeight(0);
+        persona.setWeight(0);
+        persona.setStudent(false);
+        return persona;
+    }
+
+}
