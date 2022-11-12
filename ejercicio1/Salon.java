56a639d8e360d69a042b8fffc1161454a0abd0f8
Jose Monroy <mnry7999@gmail.com>
2022-11-11 19:07:08 -0600

    metodo para la conctaenacion de strings

--- ejercicio1/Salon.java
+++ ejercicio1/Salon.java
@@ -16,5 +16,8 @@ public class Salon{
         persona.setStudent(false);
         return persona;
     }
+    static String Concatenar (String string1, String strin2){
+        return string1 + "-" + strin2;
+    }
 
 }
