b2ec1ec4bfb74155520087965171b0a51e6e7532
Jose Monroy <mnry7999@gmail.com>
2022-11-13 10:46:41 -0600

    Auxiliar para retornar doble valor

---
+++ Evaluacion/DobleRetProducto.java
@@ -0,0 +1,10 @@
+public class DobleRetProducto {
+    private Producto[] Productos;
+    private int costo;
+
+    public DobleRetProducto(Producto[] Productos, int costo){
+        this.Productos = Productos;
+        this.costo = costo;
+    }
+
+}
