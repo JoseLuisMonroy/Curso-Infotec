9b188a2e39a3eda38b38f1fbd16e1fe545bf34aa
Jose Monroy <mnry7999@gmail.com>
2022-12-02 18:44:45 -0600

    Exportacion de archivo txt

--- traductor/src/main/java/com/example/demo/diccionario.java
+++ traductor/src/main/java/com/example/demo/diccionario.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class diccionario {
     Map<String, String> diccionario = new HashMap<String, String>();
     Map<String, String> diccionario2 = new HashMap<String, String>();
-
-    @GetMapping("/traducir/{palabra}")
-    public String traducir(@PathVariable String palabra) {
-        palabra = palabra.toLowerCase();
+    Map<String, String> diccionario3 = new HashMap<String, String>();
+    @GetMapping("/")
+    public String index() {
         diccionario.put("hola", "hello");
         diccionario.put("adios", "goodbye");
         diccionario.put("perro", "dog");
@@ -28,6 +30,12 @@ public class diccionario {
         diccionario2.put("gato", "chat");
         diccionario2.put("casa", "maison");
         diccionario2.put("computadora", "ordinateur");
+        return "Hola, bienvenido al traductor";
+    }
+
+    @GetMapping("/traducir/{palabra}")
+    public String traducir(@PathVariable String palabra) {
+        palabra = palabra.toLowerCase();
         if (diccionario.containsKey(palabra)) {
         	String resp =  "La traduccion de " + palabra + " al ingles es " + diccionario.get(palabra) + " y al frances es "
                     + diccionario2.get(palabra);
@@ -49,5 +57,53 @@ public class diccionario {
         diccionario.put(palabra, traduccionIng);
         diccionario2.put(palabra, traduccionFr);
         return "La palabra " + palabra + " fue agregada al diccionario";
+    }
+
+    @GetMapping("/exportar/{idioma}")
+    public String exportar(@PathVariable String idioma) {
+        if (idioma.equals("ingles")) {
+            return this.exportarIngles();
+        } else if (idioma.equals("frances")) {
+            return this.exportarFrances();
+        } else {
+            return "El idioma no existe";
+        }
+    }
+
+    public String exportarIngles() {
+        Map<String, String> diccionarioOrdenado = new HashMap<String, String>();
+        diccionarioOrdenado.putAll(diccionario);
+        diccionarioOrdenado.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEachOrdered(x -> diccionario3.put(x.getKey(), x.getValue()));
+
+        try {
+            File file = new File("ingles-14-05-2022.txt");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            for (String key : diccionarioOrdenado.keySet()) {
+                bw.write(key + " " + diccionarioOrdenado.get(key));
+                bw.newLine();
+            }
+            bw.close();
+            return "El archivo fue exportado correctamente";
+        } catch (Exception e) {
+            return "El archivo no pudo ser exportado";
+        }
+    }
+
+    public String exportarFrances() {
+    	Map<String, String> diccionarioOrdenado = new HashMap<String, String>();
+        diccionarioOrdenado.putAll(diccionario2);
+        diccionarioOrdenado.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEachOrdered(x -> diccionario3.put(x.getKey(), x.getValue()));
+        try {
+            File file = new File("frances-14-05-2022.txt");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            for (String key : diccionarioOrdenado.keySet()) {
+                bw.write(key + " " + diccionarioOrdenado.get(key));
+                bw.newLine();
+            }
+            bw.close();
+            return "El archivo fue exportado correctamente";
+        } catch (Exception e) {
+            return "El archivo no pudo ser exportado";
+        }
     }
 }
