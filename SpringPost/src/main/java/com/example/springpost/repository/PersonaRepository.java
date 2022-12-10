b39f602e54d4a0e0133fe651adccd1dd2846e60d
Jose Monroy <mnry7999@gmail.com>
2022-12-10 11:48:30 -0600

    A ccesoa  datos

--- SpringPost/src/main/java/com/example/springpost/repository/PersonaRepository.java
+++ SpringPost/src/main/java/com/example/springpost/repository/PersonaRepository.java
@@ -1,6 +1,8 @@
 package com.example.springpost.repository;
 
+import com.example.springpost.entity.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonaRepository extends JpaRepository <> {
+public interface PersonaRepository extends JpaRepository <Persona, Integer> {
+
 }
