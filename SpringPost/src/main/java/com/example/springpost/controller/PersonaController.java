97b06dde93e129eb594c6a9925e20eba83a08e8d
Jose Monroy <mnry7999@gmail.com>
2022-12-10 12:08:01 -0600

    Put por request params

--- SpringPost/src/main/java/com/example/springpost/controller/PersonaController.java
+++ SpringPost/src/main/java/com/example/springpost/controller/PersonaController.java
@@ -1,2 +1,28 @@
-package com.example.springpost.controller;public class PersonaController {
+package com.example.springpost.controller;
+
+import com.example.springpost.service.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonaController {
+    @Autowired
+    public PersonaService personaService;
+    @PutMapping("/guardar")
+    public ResponseEntity<Integer> guardarPersona(@RequestParam("nombre")String nombre,@RequestParam("nombre")String apellidos) {
+        Integer id = 0;
+        try {
+            id = this.personaService.guardarPersona(nombre, apellidos);
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        if (id == 0) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<Integer>(id, HttpStatus.OK);
+    }
 }
--- SpringPost/src/main/java/com/example/springpost/service/PersonaService.java
+++ SpringPost/src/main/java/com/example/springpost/service/PersonaService.java
@@ -1,2 +1,25 @@
-package com.example.springpost.service;public class PersonaService {
+package com.example.springpost.service;
+
+import com.example.springpost.entity.Persona;
+import com.example.springpost.repository.PersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonaService {
+    private PersonaRepository personaRepository;
+
+    public PersonaService(PersonaRepository personaRepository){
+        this.personaRepository = personaRepository;
+    }
+    public Persona obtenerPorId(Integer id){
+        return this.personaRepository.findById(id).get;
+    }
+    public Integer guardarPersona(String nombre, String apellidos){
+        Persona persona = new Persona();
+        persona.setNombre(nombre);
+        persona.setApellidos(apellidos);
+        persona = personaRepository.save(persona);
+        return persona.getId();
+    }
 }
