9d2ab9136bedae390edabc76af474624f6c7a831
Jose Monroy <mnry7999@gmail.com>
2022-12-10 11:46:57 -0600

    Creaciond e clase persona

---
+++ SpringPost/src/main/java/com/example/springpost/entity/Persona.java
@@ -0,0 +1,42 @@
+package com.example.springpost.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "persona", schema = "public")
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellidos")
+    private String apellidos;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
--- SpringPost/src/test/java/com/example/springpost/SpringPostApplicationTests.java
+++ SpringPost/src/test/java/com/example/springpost/SpringPostApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.springpost;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PrimeraConexionApplicationTests {
+class SpringPostApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
