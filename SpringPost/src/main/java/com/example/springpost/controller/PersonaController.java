d7f031b4c9168502f55fe2411398e38de6c617f6
Jose Monroy <mnry7999@gmail.com>
2022-12-10 12:13:17 -0600

    Guardar datos por medio de una DTO

--- SpringPost/src/main/java/com/example/springpost/controller/PersonaController.java
+++ SpringPost/src/main/java/com/example/springpost/controller/PersonaController.java
@@ -1,5 +1,6 @@
 package com.example.springpost.controller;
 
+import com.example.springpost.dto.DataDTO;
 import com.example.springpost.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,19 @@ public class PersonaController {
         Integer id = 0;
         try {
             id = this.personaService.guardarPersona(nombre, apellidos);
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        if (id == 0) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<Integer>(id, HttpStatus.OK);
+    }
+    @PutMapping("/guadarDTO")
+    public ResponseEntity<Integer> guardarPersona(@RequestParam DataDTO data) {
+        Integer id = 0;
+        try {
+            id = this.personaService.guardarPersona(data.getNombre(), data.getApellidos());
         } catch (Exception e) {
             System.out.println("Error");
         }
