7735c44534facdb58ca45950756b09fc580aa86d
Jose Monroy <mnry7999@gmail.com>
2022-12-19 18:08:34 -0600

    Adecuacion para mostrar al componente dado

--- proyecto002/src/app/app.component.ts
+++ proyecto002/src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto002';
+  title = 'Mostrar dados aleatorios';
+
+  valor1: string = "";
+  valor2: string = "";
+  valor3: string = "";
+  constructor() { 
+    this.valor1 = this.valorAleatorio();
+    this.valor2 = this.valorAleatorio();
+    this.valor3 = this.valorAleatorio();
+  }
+
+  valorAleatorio(): string {
+    let valor = Math.floor(Math.random() * 6) + 1;
+    return valor.toString();
+  }
+
+  lanzar(): void {
+    this.valor1 = this.valorAleatorio();
+    this.valor2 = this.valorAleatorio();
+    this.valor3 = this.valorAleatorio();
+  }
 }
