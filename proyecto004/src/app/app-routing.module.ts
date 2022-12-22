7705a177f65169f7b9f33d45398210d3029b0f21
Jose Monroy <mnry7999@gmail.com>
2022-12-21 18:01:56 -0600

    Configuracion de rutas

--- proyecto004/src/app/app-routing.module.ts
+++ proyecto004/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { JuegoDadosComponent } from './juegoDados/juegoDados.component';
+import {AcercaDeComponent} from './acercaDe/acercaDe.component';
+
+const routes: Routes = [
+  {
+    path: 'juegoDados',
+    component: JuegoDadosComponent
+  },
+  {
+    path: 'acercaDe',
+    component: AcercaDeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
