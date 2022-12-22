1ecd012886defc1fae17e66a5e973112eb75b9b3
Jose Monroy <mnry7999@gmail.com>
2022-12-22 14:35:12 -0600

    Creacion de persona

--- proyecto006/src/app/app.component.ts
+++ proyecto006/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto006';
+  nombre = "Jose Monroy";
+  sueldo = 1000.50;
+  dias = [`domingo`, `lunes`, `martes`, `miercoles`, `jueves`, `viernes`, `sabado`];
+  articulos = [
+    { codigo: 1, descripcion: `Arroz`, precio: 10.50 },
+    { codigo: 2, descripcion: `Frijoles`, precio: 20.50 },
+  ];
+  fecha = new Date();
 }
