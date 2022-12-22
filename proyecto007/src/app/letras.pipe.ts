e61105c49774553a926b89d6d5b520eb96bcb678
Jose Monroy <mnry7999@gmail.com>
2022-12-22 14:57:34 -0600

    Reglas de pipe custom

--- proyecto007/src/app/letras.pipe.ts
+++ proyecto007/src/app/letras.pipe.ts
@@ -5,8 +5,40 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class LetrasPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: any, args?: any): any {
+      if(args != null){
+        if(args == `ingles`){
+          switch(value){
+            case 1: return `One`;
+            case 2: return `Two`;
+            case 3: return `Three`;
+            case 4: return `Four`;
+            case 5: return `Five`;
+            case 6: return `Six`;
+            case 7: return `Seven`;
+        }
+      }
+      if(args == `portugues`){
+        switch(value){
+          case 1: return `Um`;
+          case 2: return `Dois`;
+          case 3: return `Três`;
+          case 4: return `Quatro`;
+          case 5: return `Cinco`;
+          case 6: return `Seis`;
+          case 7: return `Sete`;
+        }
+      }
+      switch(value){
+        case 1: return `Uno`;
+        case 2: return `Dos`;
+        case 3: return `Tres`;
+        case 4: return `Cuatro`;
+        case 5: return `Cinco`;
+        case 6: return `Seis`;
+        case 7: return `Siete`;
+    }
     return null;
+    }
   }
-
 }
